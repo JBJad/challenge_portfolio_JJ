7d23c961582f5a0c55f264d0b933ea376f86d120
JBJad <25285354+JBJad@users.noreply.github.com>
2023-04-15 15:43:44 +0200

    selectors_tasks
    added selectors

---
+++ pages/add_a_match_form.py
@@ -0,0 +1,15 @@
+from pages.base_page import BasePage
+
+class AddAMatchForm(BasePage):
+    players_team_field_xpath = "//input[@name='myTeam']"
+    opponent_team_field_xpath = "//input[@name='enemyTeam']"
+    goals_field_xpath = "//input[@name='myTeamScore']"
+    lost_goals_field_xpath = "//input[@name='enemyTeamScore']"
+    date_field_xpath = "//input[@name='date']"
+    matchAtHome_field_xpath = "//fieldset/div/label[1]/span[1]"
+    matchOutside_field_xpath= "//fieldset/div/label[2]/span[1]"
+    shirt_colour_field_xpath = "//input[@name='tshirt']"
+    league_field_xpath = "//input[@name='league']"
+    submit_button_xpath = "//button[@type='submit']"
+    clear_button_xpath = "//form/div[3]/button[2]"
+    pass
---
+++ pages/dashboard.py
@@ -0,0 +1,16 @@
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+    main_page_hyperlink_xpath = "//ul[1]/div[1]"
+    players_page_hyperlink_xpath = "//ul[1]/div[2]"
+    language_change_to_English_xpath = "//span[text()='English']"
+    language_change_to_Polish_xpath = "//span[text()='Polski']"
+    signout_hyperlink_xpath = "//ul[2]/div[2]"
+    devteamcontact_hyperlink_xpath = "//*[text() ='Dev team contact']"
+    addplayer_hyperlink_xpath = "//div[2]/div/div/a/button"
+    playerscount_number_xpath = "//div[2]/div[1]/div/div[2]/b"
+    matchescount_number_xpath = "//div[2]/div[2]/div/div[2]/b"
+    reportscount_number_xpath = "//div[2]/div[3]/div/div[2]/b"
+    eventsscount_number_xpath = "//div[2]/div[4]/div/div[2]/b"
+    pass
